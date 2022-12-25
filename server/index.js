86f6c0daf927b558518358e676df8bba41341f3f
Xinan Rahman <xinanrahman03@gmail.com>
2022-12-25 18:29:30 -0500

    set up basic express server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-console.log("Server...");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
