7c2298c9684e3509790e3b33cf5b834c8c77d342
Xinan Rahman <xinanrahman03@gmail.com>
2022-12-25 21:17:39 -0500

    created route for creating a todo

--- server/index.js
+++ server/index.js
@@ -9,6 +9,24 @@ const pool = require("./db");
 app.use(cors());
 app.use(express.json());
 
+// ROUTES
+
+// Create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// Get all todos
+
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
