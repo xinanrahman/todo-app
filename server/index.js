df18bac1b5b7c3b317fdddf01e0ddde62f2c4bea
Xinan Rahman <xinanrahman03@gmail.com>
2023-01-02 00:16:58 -0500

    fixing dev pt 4

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const pool = require("./db");
 
 // Middleware
